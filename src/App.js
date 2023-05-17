c0a110fe4a5e986dfa84120c08cbb02c36aba712
Tiffany <tapintobeauty08@gmail.com>
2023-05-17 10:48:38 -0400

    added an editing useState to Todo.js

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App(props) {
       key={task.id}
       toggleTaskCompleted={toggleTaskCompleted}
       deleteTask={deleteTask}
+      editTask={editTask}
     />
   ));
 
@@ -44,11 +45,23 @@ function App(props) {
     });
     setTasks(updatedTasks);
   }
-  
-function deleteTask(id) {
- const remainingTasks = tasks.filter((task) => id !== task.id);
- setTasks(remainingTasks);
-}
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map((task) => {
+      //if this task has the same ID as the edited task
+      if (id === task.id) {
+
+        return { ...task, name: newName };
+      }
+        return task;
+    });
+    setTasks(editedTaskList);
+  }
 
   return (
     <div className="todoapp stack-large">
